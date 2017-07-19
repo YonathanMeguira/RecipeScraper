203d097f4ff37ddbeae65fab529c5158738882ab
jonathanmeguira <johnmeguira@gmail.com>
2017-07-19 11:37:12 +0300

    init commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var parser = require('./lib/scrapper');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -9,12 +10,16 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/', function (request, response) {
+    const url = request.query;
+    parser.parseRecipe('http://www.epicurious.com/recipes/food/views/The-Ultimate-Burger-353654')
+        .then((res) => {
+            response.send(res);
+        })
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
 });
 
 
